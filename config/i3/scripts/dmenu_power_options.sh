4fbdbde0b0c1f6dd9d02b07884a023311fa572b1
hat3ph <88069788+hat3ph@users.noreply.github.com>
2023-09-10 21:12:06 +0800

    include logout to power options

--- config/i3/scripts/dmenu_power_options.sh
+++ config/i3/scripts/dmenu_power_options.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 # my custom dmenu power options
-echo -e "poweroff\nreboot\nsuspend" | $HOME/.config/i3/scripts/my_dmenu.sh -p "Power Options:" | xargs systemctl
+#echo -e "poweroff\nreboot\nsuspend" | $HOME/.config/i3/scripts/my_dmenu.sh -p "Power Options:" | xargs systemctl
+
+choice=`echo -e "0: Logout\n1: Shutdown\n2: Suspend\n3: Reboot\n4: Cancel" | $HOME/.config/i3/scripts/my_dmenu.sh -p "Power Options:" | cut -d ':' -f 1`
+
+# execute the choice in background
+case "$choice" in
+  #0) i3-msg exit & ;;
+  0) i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -B 'Yes, exit i3' 'i3-msg exit' & ;;
+  1) systemctl poweroff & ;;
+  2) systemctl suspend & ;;
+  3) systemctl reboot & ;;
+  4) exit ;;
+esac

094072d5756b34726f8aa4aa55e1dd97ee13b84e
hat3ph <88069788+hat3ph@users.noreply.github.com>
2024-03-15 20:47:42 +0800

    install non snap firefox

--- setup.sh
+++ setup.sh
@@ -6,13 +6,15 @@ audio=yes # set no if do not want to use pipewire audio server
 extra_pkg=yes # set no if do not want to install the extra packages
 nm=yes # set no if do not want to use network-manager for network interface management
 nano_config=yes # set no if do not want to configure nano text editor
+thunar=yes # set no if do not want to install thunar file manager
+firefox_deb=yes # install non snap firefox
 
 install () {
 	# install swaywm and other packages
 	sudo apt-get update && sudo apt-get upgrade -y
 	sudo apt-get install i3 suckless-tools xorg xinit xterm feh lxappearance papirus-icon-theme fonts-font-awesome \
 		fonts-noto-color-emoji xdg-utils xdg-user-dirs policykit-1 libnotify-bin dunst nano less \
-		iputils-ping software-properties-gtk policykit-1-gnome dex -y
+		iputils-ping software-properties-gtk policykit-1-gnome dex gpicview geany gv -y
 
 	# copy my i3 configuration
 	if [[ $my_i3wm_config == "yes" ]]; then
@@ -43,9 +45,8 @@ install () {
 	fi
 
 	# optional to insstall the extra packages
-	if [[ $extra_pkg == "yes" ]]; then
-		sudo apt-get install thunar gvfs gvfs-backends thunar-archive-plugin thunar-media-tags-plugin avahi-daemon \
-			gpicview geany gv sddm -y
+	if [[ $thunar == "yes" ]]; then
+		sudo apt-get install thunar gvfs gvfs-backends thunar-archive-plugin thunar-media-tags-plugin avahi-daemon -y
 	fi
 
 	# optional install NetworkManager
@@ -61,10 +62,32 @@ install () {
 			sudo cp /etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf.bak
 			sudo sed -i 's/managed=false/managed=true/g' /etc/NetworkManager/NetworkManager.conf
 			sudo mv /etc/network/interfaces /etc/network/interfaces.bak
-			sudo cp ./config/interfaces /etc/network/interfaces
+			head -9 /etc/network/interfaces.bak | sudo tee /etc/network/interfaces
 			sudo systemctl disable networking.service
 		fi
 	fi
+
+	# install firefox without snap
+    	# https://www.omgubuntu.co.uk/2022/04/how-to-install-firefox-deb-apt-ubuntu-22-04
+	if [[ $firefox_deb == "yes" ]]; then
+		if [[ -n "$(uname -a | grep Ubuntu)" ]]; then
+			sudo install -d -m 0755 /etc/apt/keyrings
+			wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | \
+				sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+			echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | \
+				sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+			echo -e "Package: *\nPin: origin packages.mozilla.org\nPin-Priority: 1000" | \
+				sudo tee /etc/apt/preferences.d/mozilla
+			sudo apt-get update && sudo apt-get install firefox -y
+		else
+			sudo apt-get install firefox-esr -y
+			sed -i 's/firefox/firefox-esr/g' $HOME/.icewm/{menu,toolbar}
+		fi
+  	fi
+
+	# disable unused services
+ 	sudo systemctl disable systemd-networkd-wait-online.service
+  	sudo systemctl disable multipathd.service
 
 }
 
@@ -73,7 +96,8 @@ printf "Start installation!!!!!!!!!!!\n"
 printf "88888888888888888888888888888\n"
 printf "My Custom i3WM Config   : $my_i3wm_config\n"
 printf "Pipewire Audio          : $audio\n"
-printf "Extra Packages          : $extra_pkg\n"
+printf "Thunar File Manager     : $thunar\n"
+printf "Firefox (non snap)      : $firefox_deb\n"
 printf "NetworkManager          : $nm\n"
 printf "Nano's configuration    : $nano_config\n"
 printf "88888888888888888888888888888\n"
